84eefce6ea9790f1717a4281cbf95ebd6196a74c
jaymehta638 <jaykmehta2006@gmail.com>
2026-06-15 15:55:05 +0530

    v10

--- main.go
+++ main.go
@@ -10,11 +10,6 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "static/home.html")
 }
 
-func coursePage(w http.ResponseWriter, r *http.Request) {
-	// Render the course html page
-	http.ServeFile(w, r, "static/courses.html")
-}
-
 func aboutPage(w http.ResponseWriter, r *http.Request) {
 	// Render the about html page
 	http.ServeFile(w, r, "static/about.html")
@@ -27,13 +22,15 @@ func contactPage(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 
-	http.HandleFunc("/home", homePage)
-	http.HandleFunc("/courses", coursePage)
-	http.HandleFunc("/about", aboutPage)
-	http.HandleFunc("/contact", contactPage)
+http.HandleFunc("/", homePage)
+http.HandleFunc("/about", aboutPage)
+http.HandleFunc("/contact", contactPage)
 
-	err := http.ListenAndServe("0.0.0.0:8080", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+log.Println("Server started on :8080")
+
+err := http.ListenAndServe(":8080", nil)
+if err != nil {
+	log.Fatal(err)
+}
+
 }
